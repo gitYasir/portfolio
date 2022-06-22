b6eec63cf11b25711bac193c546c37a05dc297ad
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-06-22 03:10:58 +0100

    downloaded some logo svgs and centered content

--- Components/Splash/Splash.js
+++ Components/Splash/Splash.js
@@ -1,15 +1,19 @@
 import Image from "next/image";
 import css from "./Splash.module.scss";
+import linkedIn from "../../public/linkedin.svg";
 
 function Splash() {
   return (
     <div className={css.splash}>
-      <div>
-        <h1>
-          HI, MY NAME IS IBRAHIM. I AM A HARD WORKING AND AMBITIOUS WEB
-          DEVELOPER LOOKING FOR ENTRY-LEVEL ROLE AS A FRONTEND OR BACKEND
+      <div className={css.info}>
+        <h3>
+          HI, MY NAME IS YASIR IBRAHIM. I AM A HARD WORKING AND AMBITIOUS WEB
+          DEVELOPER LOOKING FOR AN ENTRY-LEVEL ROLE AS A FRONTEND OR BACKEND
           DEVELOPER.
-        </h1>
+        </h3>
+        <div>
+          <Image src={linkedIn} alt="LinkedIn Logo" height={25} width={25} />
+        </div>
       </div>
     </div>
   );
