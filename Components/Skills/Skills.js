f80a926a2bd046b61227be18c237dfc38e76ff6b
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-06-23 19:15:09 +0100

    working on the skills component

--- Components/Skills/Skills.js
+++ Components/Skills/Skills.js
@@ -3,7 +3,9 @@ import css from "./Skills.module.scss";
 function Skills() {
   return (
     <section className={css.mainContainer}>
-      <div></div>
+      <div className={css.techSkills}>
+        <h3 className={css.title}>Skills & Technologies</h3>
+      </div>
     </section>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Splash from "../Components/Splash/Splash";
 import Navbar from "../Components/NavBar/Navbar";
 import AboutMe from "../Components/AboutMe/AboutMe";
+import Skills from "../Components/Skills/Skills";
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
       <Navbar />
       <Splash />
       <AboutMe />
+      <Skills />
     </>
   );
 }
