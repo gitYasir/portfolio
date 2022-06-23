c1e94700391379a0d988888f0746eef3de432025
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-06-23 02:58:32 +0100

    added my pic to about me page, styled it

--- Components/AboutMe/AboutMe.js
+++ Components/AboutMe/AboutMe.js
@@ -1,9 +1,19 @@
 import css from "./AboutMe.module.scss";
-
+import Image from "next/image";
+import myPic from "../../public/myPic.jpeg";
 function AboutMe() {
   return (
     <section className={css.aboutMeSec}>
-      <div>Pic</div>
+      <div className={css.picContainer}>
+        <Image
+          src={myPic}
+          alt={"Yasir Ibrahim"}
+          className={css.pic}
+          height={150}
+          width={170}
+          layout="fill"
+        />
+      </div>
       <div>About me</div>
       <div>Contact</div>
       <div>CV</div>
