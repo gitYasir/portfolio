45499c5774d7327926669c27e6fc4c1c98b8f0c9
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-06-23 18:24:42 +0100

    started work on the skills component

--- Components/Skills/Skills.js
+++ Components/Skills/Skills.js
@@ -1,1 +1,11 @@
 import css from "./Skills.module.scss";
+
+function Skills() {
+  return (
+    <section className={css.mainContainer}>
+      <div></div>
+    </section>
+  );
+}
+
+export default Skills;
