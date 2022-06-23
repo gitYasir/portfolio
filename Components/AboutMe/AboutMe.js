2fb55060399b59f6b87bd2c7c49ea8c7dce2d837
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-06-23 03:35:21 +0100

    added info to about me section

--- Components/AboutMe/AboutMe.js
+++ Components/AboutMe/AboutMe.js
@@ -4,19 +4,30 @@ import myPic from "../../public/myPic.jpeg";
 function AboutMe() {
   return (
     <section className={css.aboutMeSec}>
-      <div className={css.picContainer}>
-        <Image
-          src={myPic}
-          alt={"Yasir Ibrahim"}
-          className={css.pic}
-          height={150}
-          width={170}
-          layout="fill"
-        />
+      <div className={css.textArea}>
+        <div className={css.picContainer}>
+          <Image
+            src={myPic}
+            alt={"Yasir Ibrahim"}
+            className={css.pic}
+            height={150}
+            width={170}
+            layout="fill"
+          />
+        </div>
+        <div className={css.aboutMe}>
+          <h3>About Me</h3>
+          <p>
+            After 6 years of studying medicine in China, I realised my passion
+            was actually in technology and building apps from scratch. I am
+            looking for a junior developer role that will allow me to blend my
+            creativity, programming & problem-solving skills.
+          </p>
+          {/* add a 'more' button that leads to a more in-depth about me */}
+        </div>
+        <div>Contact</div>
+        <div>CV</div>
       </div>
-      <div>About me</div>
-      <div>Contact</div>
-      <div>CV</div>
     </section>
   );
 }
