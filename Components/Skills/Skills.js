b6c37ab8234107200a4a9175c4fd5461d42c0d99
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-06-24 02:39:31 +0100

    got icons ready

--- Components/Skills/Skills.js
+++ Components/Skills/Skills.js
@@ -1,10 +1,23 @@
 import css from "./Skills.module.scss";
-
+import ui from "../../public/Icons/nextUIIcon.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDownload } from "@fortawesome/free-solid-svg-icons";
+import Image from "next/image";
+import {
+  faJsSquare,
+  faHtml5,
+  faSass,
+  faReact,
+  faNodeJs,
+  faGit,
+} from "@fortawesome/free-brands-svg-icons";
 function Skills() {
   return (
     <section className={css.mainContainer}>
       <div className={css.techSkills}>
-        <h3 className={css.title}>Skills & Technologies</h3>
+        <div className={css.titleArea}>
+          <h3 className={css.title}>Skills & Technologies</h3>
+        </div>
       </div>
     </section>
   );
---
+++ Components/Skills/SkillsIconsDB.js
@@ -0,0 +1,37 @@
+import auth0 from "../../public/Icons/auth0-svgrepo-com.svg";
+import express from "../../public/Icons/express-svgrepo-com.svg";
+import gitActions from "../../public/Icons/githubactions-svgrepo-com.svg";
+import heroku from "../../public/Icons/heroku-logo-svgrepo-com.svg";
+import jest from "../../public/Icons/jest-svgrepo-com.svg";
+import js from "../../public/Icons/js-svgrepo-com.svg";
+import next from "../../public/Icons/nextjs-svgrepo-com.svg";
+import nextUI from "../../public/Icons/nextUIIcon.svg";
+import nodeJS from "../../public/Icons/nodejs-svgrepo-com.svg";
+import postgreSQL from "../../public/Icons/postgresql-svgrepo-com.svg";
+import postman from "../../public/Icons/postman-icon-svgrepo-com.svg";
+import react from "../../public/Icons/react-svgrepo-com.svg";
+import html from "../../public/Icons/html-svgrepo-com.svg";
+import sass from "../../public/Icons/sass-svgrepo-com.svg";
+import git from "../../public/Icons/git-svgrepo-com.svg";
+import netlify from "../../public/Icons/netlify-svgrepo-com.svg";
+
+const skillsIcons = [
+  { icon: js, name: "JavaScript" },
+  { icon: html, name: "HTML5" },
+  { icon: sass, name: "SASS/CSS" },
+  { icon: react, name: "React" },
+  { icon: nodeJS, name: "Node JS" },
+  { icon: git, name: "Git" },
+  { icon: auth0, name: "Auth0" },
+  { icon: jest, name: "Jest" },
+  { icon: netlify, name: "Netlify" },
+  { icon: heroku, name: "Heroku" },
+  { icon: postgreSQL, name: "PostgreSQL" },
+  { icon: next, name: "Next.js" },
+  { icon: express, name: "Express JS" },
+  { icon: gitActions, name: "GitHub Actions" },
+  { icon: nextUI, name: "NextUI" },
+  { icon: postman, name: "Postman" },
+];
+
+export default skillsIcons;
