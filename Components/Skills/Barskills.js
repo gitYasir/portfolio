a45e5366bfc3f5f0ed47290524b9f9f367f51be6
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-06-24 04:09:08 +0100

    added animated bars for soft skils

---
+++ Components/Skills/Barskills.js
@@ -0,0 +1,8 @@
+const softSkills = [
+  { type: "Adaptability", level: 99 },
+  { type: "Team work", level: 95 },
+  { type: "Orgnisation", level: 95 },
+  { type: "Communication", level: 90 },
+];
+
+export default softSkills;
--- Components/Skills/Skills.js
+++ Components/Skills/Skills.js
@@ -1,7 +1,8 @@
 import css from "./Skills.module.scss";
 import skillsIcons from "./SkillsIconsDB";
 import Image from "next/image";
-
+import SkillBar from "react-skillbars";
+import softSkills from "./Barskills";
 function Skills() {
   return (
     <section className={css.mainContainer}>
@@ -25,6 +26,14 @@ function Skills() {
               </div>
             );
           })}
+        </div>
+      </div>
+      <div className={css.softSkills}>
+        <div className={css.titleArea}>
+          <h3>Soft Skills</h3>
+        </div>
+        <div className={css.barArea}>
+          <SkillBar skills={softSkills} height={"40px"} animationDelay={300} />
         </div>
       </div>
     </section>
