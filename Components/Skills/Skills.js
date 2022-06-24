52d890dc724b75fdefa2ff2e7724a150cbcb4ea4
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-06-24 03:43:11 +0100

    skills section done

--- Components/Skills/Skills.js
+++ Components/Skills/Skills.js
@@ -1,22 +1,30 @@
 import css from "./Skills.module.scss";
-import ui from "../../public/Icons/nextUIIcon.svg";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDownload } from "@fortawesome/free-solid-svg-icons";
+import skillsIcons from "./SkillsIconsDB";
 import Image from "next/image";
-import {
-  faJsSquare,
-  faHtml5,
-  faSass,
-  faReact,
-  faNodeJs,
-  faGit,
-} from "@fortawesome/free-brands-svg-icons";
+
 function Skills() {
   return (
     <section className={css.mainContainer}>
       <div className={css.techSkills}>
         <div className={css.titleArea}>
           <h3 className={css.title}>Skills & Technologies</h3>
+        </div>
+        <div className={css.skills}>
+          {skillsIcons.map((icon) => {
+            return (
+              <div key={Math.random()} className={css.iconContainer}>
+                <div className={css.iconArea}>
+                  <Image
+                    src={icon.icon}
+                    alt={icon.name}
+                    className={css.icon}
+                    layout="fill"
+                  />
+                </div>
+                <h5>{icon.name}</h5>
+              </div>
+            );
+          })}
         </div>
       </div>
     </section>
