88c021e0029005ffa245511162200d870d813811
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-06-26 19:37:30 +0100

    making progress with the project card

--- Components/Projects/Project.js
+++ Components/Projects/Project.js
@@ -1,21 +1,27 @@
 import Image from "next/image";
 import React from "react";
 import css from "./Projects.module.scss";
+import picc from "../../public/projects/finalProject1.jpg";
+
 function Project({ pic, name, live, code, description }) {
   return (
     <div className={css.projectCard}>
       <div className={css.textAndLinkArea}>
-        <p>text here</p>
-        <button>Live</button>
-        <button>code</button>
+        <ul>
+          <li>bullet point </li>
+          <li>bullet point </li>
+          <li>bullet point </li>
+          <li>bullet point </li>
+          <li>bullet point </li>
+          <li>bullet point </li>
+        </ul>
+        <div className={css.btn}>
+          <button>Live</button>
+          <button>code</button>
+        </div>
       </div>
       <div className={css.picArea}>
-        <Image
-          src={"/public/projects/finalProject1.jpg"}
-          alt=""
-          height={"100px"}
-          width={"100px"}
-        />
+        <Image src={picc} alt="" layout="fill" className={css.screenShot} />
       </div>
     </div>
   );
