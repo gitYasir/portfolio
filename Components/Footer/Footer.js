ce95b295e151b9ae214eda900712bbefc60f37bb
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-06-28 13:17:38 +0100

    added links to footer icons

--- Components/Footer/Footer.js
+++ Components/Footer/Footer.js
@@ -1,21 +1,30 @@
 import css from "./Footer.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+import Link from "next/link";
 
 function Footer() {
   return (
     <div className={css.container}>
       <div className={css.extLinks}>
-        <FontAwesomeIcon
-          icon={faLinkedinIn}
-          style={{ width: "40px", height: "40px" }}
-          className={css.icon}
-        />
-        <FontAwesomeIcon
-          icon={faGithub}
-          style={{ width: "40px", height: "40px" }}
-          className={css.icon}
-        />
+        <Link href={"https://www.linkedin.com/in/yasir-ibrahim/"}>
+          <a target="_blank">
+            <FontAwesomeIcon
+              icon={faLinkedinIn}
+              style={{ width: "40px", height: "40px" }}
+              className={css.icon}
+            />
+          </a>
+        </Link>
+        <Link href={"https://github.com/gitYasir"}>
+          <a target="_blank">
+            <FontAwesomeIcon
+              icon={faGithub}
+              style={{ width: "40px", height: "40px" }}
+              className={css.icon}
+            />
+          </a>
+        </Link>
       </div>
       <h5>Site developed by Yasir Ibrahim using Next.js</h5>
     </div>
